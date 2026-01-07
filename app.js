85666ab4b6eeaa7c552fb44649af35fe0cddbe57
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2026-01-07 21:16:47 +0530

    qr code made

---
+++ app.js
@@ -0,0 +1,14 @@
+const qr = document.getElementById('qr');
+const input = document.getElementById('input');
+const btn = document.getElementById('btn')
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  const value = input.value
+  
+  new QRCode(qr, {
+    text: `${value}`,
+  })
+})
+
